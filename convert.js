0a68c66dc8e59685e4f54d92168dbc58f4d2ce4f
sonalsai <sonalsai777@gmail.com>
2023-08-25 16:43:11 +0530

    First commit

---
+++ convert.js
@@ -0,0 +1,24 @@
+function inrtousd()
+{
+    document.getElementById("usd").value = document.getElementById("inr").value*0.012;
+}
+function usdtoinr()
+{
+    document.getElementById("inr1").value = document.getElementById("usd1").value*83.16;
+}
+function cadtoeuro()
+{
+    document.getElementById("euro").value = document.getElementById("cad").value*0.68;
+}
+function eurotocad()
+{
+    document.getElementById("cad1").value = document.getElementById("euro1").value*1.47;
+}
+function yentopound()
+{
+    document.getElementById("pound").value = document.getElementById("yen").value*0.21;
+}
+function poundtoyen()
+{
+    document.getElementById("yen1").value = document.getElementById("pound1").value*4.72;
+}
