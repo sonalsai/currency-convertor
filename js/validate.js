1aed8e8e80d5d3b1a64064798201f49d1bd84126
sonalsai <sonalsai777@gmail.com>
2023-08-26 16:09:19 +0530

    third commit

--- js/validate.js
+++ js/validate.js
@@ -2,32 +2,48 @@ function validateForm() {
     var name = document.forms["frmval"]["name"].value;
     var email = document.forms["frmval"]["email"].value;
     var message = document.forms["frmval"]["message"].value;
+    var isValid = true;
 
     if (name === "") {
-        alert("Name must be filled out");
-        return false;
+        displayError("nameError", "Fill the Name Box");
+        isValid = false;
+    } else {
+        hideError("nameError");
     }
 
     if (email === "") {
-        alert("Email must be filled out");
-        return false;
+        displayError("emailError", "Fill the Email Box");
+        isValid = false;
     } else if (!isValidEmail(email)) {
-        alert("Invalid email format");
-        return false;
+        displayError("emailError", "Invalid Email format");
+        isValid = false;
+    } else {
+        hideError("emailError");
     }
 
     if (message === "") {
-        alert("message must be filled out");
-        return false;
-    } else if (isNaN(message)) {
-        alert("message must be a number");
-        return false;
+        displayError("messageError", "Fill the Message Box");
+        isValid = false;
+    } else {
+        hideError("messageError");
     }
 
-    return true;
+    return isValid;
 }
 
 function isValidEmail(email) {
     var pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return pattern.test(email);
+}
+
+function displayError(id, message) {
+    var errorElement = document.getElementById(id);
+    errorElement.innerHTML = message;
+    errorElement.style.display = "block";
+}
+
+function hideError(id) {
+    var errorElement = document.getElementById(id);
+    errorElement.innerHTML = "";
+    errorElement.style.display = "none";
 }
