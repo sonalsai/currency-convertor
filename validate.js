a8f5794fff8e38365adf1dfa08dd2d6332add204
sonalsai <sonalsai777@gmail.com>
2023-08-26 11:18:01 +0530

    second commit

---
+++ validate.js
@@ -0,0 +1,6 @@
+function val(){
+    if (document.getElementById("name")!="" & document.getElementById("email")!="" & document.getElementById("message")!="")
+    {
+     alert("Thanks For Contacting Us")
+    }
+}
